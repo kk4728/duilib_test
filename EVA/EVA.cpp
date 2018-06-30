60214d61860d046805eb54726decf14d0b30dede
kk4728 <gongrong816@126.com>
2018-06-30 17:01:15 +0800

    add some EVA UI.

--- EVA/EVA.cpp
+++ EVA/EVA.cpp
@@ -3,7 +3,7 @@
 
 #include "stdafx.h"
 #include "EVA.h"
-#include "../include/dui_base.h"
+
 
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
@@ -11,7 +11,19 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
                      LPTSTR    lpCmdLine,
                      int       nCmdShow)
 {
+	CPaintManagerUI::SetInstance(hInstance);
 
+	HRESULT hr = ::CoInitialize(NULL); //exc13
+	if(FAILED(hr)) return 0; //exc13
+
+	//CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath()); //exc6
+
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.CenterWindow();
+	duiFrame.ShowModal();
+	
+	::CoUninitialize(); //exc13
 
 	return 0;
 }
--- EVA/EVA.h
+++ EVA/EVA.h
@@ -1,3 +1,16 @@
 #pragma once
 
-#include "resource.h"
+//#include "resource.h"
+
+#include "../include/dui_base.h"
+
+
+class CDuiFrameWnd : public WindowImplBase
+{
+public:
+	virtual LPCTSTR    GetWindowClassName() const   {   return _T("DUIMainFrame");  }
+	virtual CDuiString GetSkinFile()                {   
+		return _T("..\\EVA\\eva.xml");    
+	}
+	virtual CDuiString GetSkinFolder()              {   return _T("");  }
+};
